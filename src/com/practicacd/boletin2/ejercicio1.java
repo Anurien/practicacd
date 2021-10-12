fa80507e981cae0ab65ae9c3a0103097828395cb
Nuria Garcia Cobas <ngarciacobas@danielcastelao.org>
2021-10-12 16:48:38 +0200

    ejercicio1 cambio de valor de las variables.

--- src/com/practicacd/boletin2/ejercicio1.java
+++ src/com/practicacd/boletin2/ejercicio1.java
@@ -2,7 +2,7 @@ package com.practicacd.boletin2;
 
 public class ejercicio1 {
     public static void main(String[] args) {
-        float lado=8;
+        float lado=4;
         float area=lado*lado;
         System.out.println("el area del cuadrado es "+lado);
 }
